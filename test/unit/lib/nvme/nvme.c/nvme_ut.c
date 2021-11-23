061fcaf07c3d7ba32d0e3a6c46a702918fb42b32
Josh Soref <jsoref@users.noreply.github.com>
2021-11-23 10:05:51 -0500

    spelling: dereferenced
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- test/unit/lib/nvme/nvme.c/nvme_ut.c
+++ test/unit/lib/nvme/nvme.c/nvme_ut.c
@@ -46,7 +46,7 @@ DEFINE_STUB_V(nvme_ctrlr_proc_put_ref, (struct spdk_nvme_ctrlr *ctrlr));
 DEFINE_STUB_V(nvme_ctrlr_fail, (struct spdk_nvme_ctrlr *ctrlr, bool hotremove));
 DEFINE_STUB(spdk_nvme_transport_available_by_name, bool,
 	    (const char *transport_name), true);
-/* return anything non-NULL, this won't be deferenced anywhere in this test */
+/* return anything non-NULL, this won't be dereferenced anywhere in this test */
 DEFINE_STUB(nvme_ctrlr_get_current_process, struct spdk_nvme_ctrlr_process *,
 	    (struct spdk_nvme_ctrlr *ctrlr), (struct spdk_nvme_ctrlr_process *)(uintptr_t)0x1);
 DEFINE_STUB(nvme_ctrlr_process_init, int,
