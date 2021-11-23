d667c514878264384cb50a670cc2b29efbc601a4
Josh Soref <jsoref@users.noreply.github.com>
2021-11-23 10:07:44 -0500

    spelling: occurring
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- test/unit/lib/blob/blob.c/blob_ut.c
+++ test/unit/lib/blob/blob.c/blob_ut.c
@@ -6864,7 +6864,7 @@ blob_persist_test(void)
 	SPDK_CU_ASSERT_FATAL(blob->active.num_pages + blob->active.num_extent_pages == page_count_xattr);
 	SPDK_CU_ASSERT_FATAL(blob->clean.num_pages + blob->clean.num_extent_pages == page_count_xattr);
 
-	/* Add xattr to a blob and sync it. While sync is occuring, remove the xattr and sync again.
+	/* Add xattr to a blob and sync it. While sync is occurring, remove the xattr and sync again.
 	 * Interrupt the first sync after increasing number of poller iterations, until it succeeds.
 	 * Expectation is that after second sync completes no xattr is saved in metadata. */
 	poller_iterations = 1;
