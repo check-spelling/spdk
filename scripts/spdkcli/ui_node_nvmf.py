fe56453670d86ad6fea2271a613f44c571d2d18c
Josh Soref <jsoref@users.noreply.github.com>
2021-11-23 10:08:53 -0500

    spelling: subsystem
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- scripts/spdkcli/ui_node_nvmf.py
+++ scripts/spdkcli/ui_node_nvmf.py
@@ -96,7 +96,7 @@ class UINVMfSubsystems(UINode):
         """Delete subsystem with given nqn.
 
         Arguments:
-            nqn_subsystem - Name of susbsytem to delete
+            nqn_subsystem - Name of subsystem to delete
         """
         self.delete(subsystem_nqn)
 
